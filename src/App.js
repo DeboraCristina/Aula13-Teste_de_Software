b433076cb1092bfb55cfa680a0572e2d14021772
Débora Cristina <98670611+DeboraCristina@users.noreply.github.com>
2024-05-17 17:54:16 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ControladorTemperatura from './componentes/controladorTemperatura';
 function App() {
   return (
     <div className="App">
-      <ControladorTemperatura/>
+      <ControladorTemperatura />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import ControladorTemperatura from "./componentes/controladorTemperatura";
 
-test('renders learn react link', () => {
-  // dado que o componente foi renderizado
+test("req01ct01 - verificar se o titulo foi apresentado na integração", () => {
+  //dado que o componente foi inicializado
   render(<App />);
-  // quando consulto o titulo
-  const textElement = screen.getByText(/Controlador de Temperatura/i);
-  // então retorna true
+  //quando cosulto a ocorrencia do titulo na tela
+  const textElement = screen.getByText(/controlador de temperatura/i);
+  //retorna titulo encontrado
   expect(textElement).toBeInTheDocument();
+});
+
+test("req01ct02- verifica o estado do contador na inicializacao", () => {
+  //dado que o componente foi inicializado
+  render(<ControladorTemperatura />);
+  //quando consulto o valor inicial da temperatura
+  const valorContador = Number(screen.getByTestId("temperatura").textContent);
+  //entao a temperatura deve ser zero
+  expect(valorContador).toBe(0);
 });
